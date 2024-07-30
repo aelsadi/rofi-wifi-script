4edf0259b79ea2d5c7394fb043bdd4519aa5eba7
aelsadi <aelsadi@protonmail.com>
2024-07-30 14:51:32 -0400

    reload list when wifi enabled

--- script.sh
+++ script.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-notify-send "Wifi" "Getting list of Wifi networks..."
-wifi_inuse=$(nmcli -t -f security,bars,in-use,ssid,bssid device wifi list | grep \* | sed 's/^.*\*:/ /g' | sed 's/.\{23\}$//')
-wifi_list=$(nmcli -t -f security,bars,in-use,ssid,bssid device wifi list | sed '/:.*::/d' | sed '/\*/d' | sed 's/^:▂▄▆█: :/󰤨 /g' | sed 's/^:▂▄▆_: :/󰤥 /g' |sed 's/^:▂▄__: :/󰤟 /g' | sed 's/^:▂___: :/󰤯 /g'| sed 's/.*:▂▄▆█: :/󰤪 /g' | sed 's/.*:▂▄▆_: :/󰤧 /g' |  sed 's/.*:▂▄__: :/󰤡 /g' | sed 's/.*:▂___: :/󰤬 /g' | sed 's/.\{23\}$//')
+  repeat=true
+  while [[ $repeat == true ]]
+  do
+    notify-send "Wifi" "Getting list of Wifi networks..."
+    wifi_inuse=$(nmcli -t -f security,bars,in-use,ssid,bssid device wifi list | grep \* | sed 's/^.*\*:/ /g' | sed 's/.\{23\}$//')
+    wifi_list=$(nmcli -t -f security,bars,in-use,ssid,bssid device wifi list | sed '/:.*::/d' | sed '/\*/d' | sed 's/^:▂▄▆█: :/󰤨 /g' | sed 's/^:▂▄▆_: :/󰤥 /g' |sed 's/^:▂▄__: :/󰤟 /g' | sed 's/^:▂___: :/󰤯 /g'| sed 's/.*:▂▄▆█: :/󰤪 /g' | sed 's/.*:▂▄▆_: :/󰤧 /g' |  sed 's/.*:▂▄__: :/󰤡 /g' | sed 's/.*:▂___: :/󰤬 /g' | sed 's/.\{23\}$//')
 
-connected=$(nmcli radio wifi)
-if [[ "$connected" =~ "enabled" ]]; then
-  wifi_status="󰤭 Disable Wifi"
-elif [[ "$connected" =~ "disabled" ]]; then
-  wifi_status="󰤨 Enable Wifi"
-fi 
-if [[ -n $wifi_inuse ]]; then
-  choosen_wifi=$(echo -e "$wifi_status\n$wifi_inuse\n$wifi_list" | uniq -s 4 | rofi -dmenu -i -p "Select a network")
-else
-  choosen_wifi=$(echo -e "$wifi_status\n$wifi_list" | uniq -s 4 | rofi -dmenu -i -p "Select a network")
-fi
+    connected=$(nmcli radio wifi)
+    if [[ "$connected" =~ "enabled" ]]; then
+      wifi_status="󰤭 Disable Wifi"
+    elif [[ "$connected" =~ "disabled" ]]; then
+      wifi_status="󰤨 Enable Wifi"
+    fi 
+    if [[ -n $wifi_inuse ]]; then
+      choosen_wifi=$(echo -e "$wifi_status\n$wifi_inuse\n$wifi_list" | uniq -s 4 | rofi -dmenu -i -p "Select a network")
+    else
+      choosen_wifi=$(echo -e "$wifi_status\n$wifi_list" | uniq -s 4 | rofi -dmenu -i -p "Select a network")
+    fi
+
+    if [[ $choosen_wifi =~ "󰤨 Enable Wifi" ]]; then
+      nmcli radio wifi on
+      nmcli device wifi rescan
+      repeat=true
+    else
+      repeat=false
+    fi
+  done
 
 if [[ $choosen_wifi =~ "󰤭 Disable Wifi" ]]; then
   nmcli radio wifi off
-elif [[ $choosen_wifi =~ "󰤨 Enable Wifi" ]]; then
-  nmcli radio wifi on
 elif [[ -n $choosen_wifi ]]; then
   if [[ $(echo $choosen_wifi | grep ) ]]; then
     if [[ $(nmcli -f name,autoconnect c s | grep -m 1 "${choosen_wifi:2}") =~ yes ]]; then
